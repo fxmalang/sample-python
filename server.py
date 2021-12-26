5b48d19815446bbbbc8c8878b4ed97454a27eb3c
Bambang Saputro <m.bambangsaputro@gmail.com>
2021-12-26 15:16:02 +0700

    test

--- server.py
+++ server.py
@@ -1,19 +1,38 @@
-import os
-import http.server
-import socketserver
-
-from http import HTTPStatus
+import asyncio
+from aiohttp import web
 
 
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(HTTPStatus.OK)
-        self.end_headers()
-        msg = 'Hello! you requested %s' % (self.path)
-        self.wfile.write(msg.encode())
+async def Task1():
+    for i in range(100):
+        print ('Task-1',i)
+        await asyncio.sleep(1)
 
 
-port = int(os.getenv('PORT', 80))
-print('Listening on port %s' % (port))
-httpd = socketserver.TCPServer(('', port), Handler)
-httpd.serve_forever()
+
+async def handle(request):
+    name = request.match_info.get('name', "Anonymous")
+    text = "Hello, " + name
+    return web.Response(text=text)
+
+async def HttspServer():
+    # create the application, as before
+    app = web.Application()
+    app.add_routes([
+        web.get('/', handle),
+        web.get('/{name}', handle)
+    ])
+
+    # set up the web server
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner)
+    await site.start()
+
+    # wait forever, running both the web server and the tasks
+    await asyncio.Event().wait()
+
+
+loop=asyncio.new_event_loop()
+loop.create_task(Task1())
+loop.create_task(HttspServer())
+loop.run_forever()
